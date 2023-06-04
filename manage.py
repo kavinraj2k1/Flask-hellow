ec626c488186ee1b96cd7f0a19755084664237a6
kavinraj2k1 <99712519+kavinraj2k1@users.noreply.github.com>
2023-06-04 10:52:14 +0530

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,35 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User,Role,Review
+
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+#creating app instance
+app = create_app('test')
+# app =create_app("development")
+
+manager= Manager(app)
+manager.add_command('server', Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Role=Role )
+
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+
+if __name__=='__main__':
+    manager.run()
